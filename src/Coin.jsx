a4398b4ba1b00aa03937cf98b17ba956ea6f09d7
Rahul R <rahul.pld007@gmail.com>
2022-12-29 23:02:11 +0530

    final commit

--- src/Coin.jsx
+++ src/Coin.jsx
@@ -15,12 +15,13 @@ const Coin = ({
       <div className="coin-row">
         <div className="coin">
           <img src={image} alt="crypto" />
+
           <h1>{name}</h1>
-          <p className="coin-symbol">{symbol}</p>
+          <p className="coin-symbol">({symbol})</p>
         </div>
         <div className="coin-data">
-          <p className="coin-price">${price}</p>
-          <p className="coin-volume">${volume.toLocaleString()}</p>
+          <p className="coin-price">Price ${price}</p>
+          <p className="coin-volume">Volume ${volume.toLocaleString()}</p>
           {priceChange < 0 ? (
             <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
           ) : (
